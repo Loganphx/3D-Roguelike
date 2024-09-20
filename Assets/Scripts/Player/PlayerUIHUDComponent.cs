8b3a5fcf20fd7463ba19440e6e60060673d3c08b
Logan Ingram <loganphx1@gmail.com>
2024-09-20 13:49:29 -0500

    - Updated PlayerUIHUDComponent to support shield

--- Assets/Scripts/Player/PlayerUIHUDComponent.cs
+++ Assets/Scripts/Player/PlayerUIHUDComponent.cs
@@ -6,23 +6,32 @@ internal class PlayerUIHUDComponent
 {
   private readonly TMP_Text _healthText;
   private readonly Image _healthFill;
-  private readonly Image _staminaFillBar;
+  private readonly Image _shieldFill;
+  private readonly Image _staminaFill;
   private readonly Image _hungerFill;
 
-  public PlayerUIHUDComponent(TMP_Text healthText, Image healthFill, Image staminaFillBar, Image hungerFill)
+  public PlayerUIHUDComponent(TMP_Text healthText, Image healthFill, Image shieldFill,
+    Image staminaFillBar, Image hungerFill)
   {
     _healthText = healthText;
     _healthFill = healthFill;
-    _staminaFillBar = staminaFillBar;
+    _shieldFill = shieldFill;
+    _staminaFill = staminaFillBar;
     _hungerFill = hungerFill;
   }
     
   public void OnUpdate(ref PlayerHealthState healthState, ref PlayerStaminaState staminaState, ref PlayerHungerState hungerState)
   {
-    _healthText.text = $"{healthState.Health} / {healthState.MaxHealth}";
+    _healthText.text = healthState.Shield > 0 
+      ? $"<color=#5DdfF1>{healthState.Health+healthState.Shield}</color> / {healthState.MaxHealth}"
+      : $"{healthState.Health} / {healthState.MaxHealth}";
+    
     _healthText.color = Color.Lerp(Color.red, Color.green, healthState.Health / (float)healthState.MaxHealth);
     _healthFill.fillAmount = healthState.Health / (float)healthState.MaxHealth;
-    _staminaFillBar.fillAmount = staminaState.Stamina / staminaState.MaxStamina;
+    _shieldFill.fillAmount = healthState.Shield / (float)healthState.MaxHealth;
+   
+    
+    _staminaFill.fillAmount = staminaState.Stamina / staminaState.MaxStamina;
     _hungerFill.fillAmount = hungerState.Hunger / hungerState.MaxHunger;
         
   }
