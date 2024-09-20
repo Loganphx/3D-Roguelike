338fc2638b47ced5a8850b0ae3750dc0add46c5f
Logan Ingram <loganphx1@gmail.com>
2024-09-20 13:47:48 -0500

    - Added Shield and Armor to the PlayerHealthComponent

--- Assets/Scripts/Player/PlayerHealthComponent.cs
+++ Assets/Scripts/Player/PlayerHealthComponent.cs
@@ -4,6 +4,11 @@ internal struct PlayerHealthState : IState
 {
     public int Health;
     public int MaxHealth;
+    
+    public int Shield;
+    public int MaxShield;
+    
+    public int Armor;
 
     public bool IsDead;
 
@@ -15,17 +20,52 @@ internal class PlayerHealthComponent : IComponent<PlayerHealthState>
     public PlayerHealthState _healthState;
 
     // ReSharper disable once UnusedParameter.Local
-    public PlayerHealthComponent(IPlayer player, int maxHealth)
+    public PlayerHealthComponent(IPlayer player, int maxHealth, int maxShield = 0)
     {
         ref var state = ref _healthState;
-        state.MaxHealth = maxHealth;
+        
         state.Health = maxHealth;
+        state.MaxHealth = maxHealth;
+
+        state.Shield = maxShield;
+        state.MaxShield = maxShield;
+
+        state.HasChanged = true;
     }
 
     public void TakeDamage(int damage)
     {
         ref var state = ref _healthState;
-        state.Health -= damage;
+
+        // Shield = 50, Damage = 100 (50-100) = -(-50)
+        // Shield = 40, Damage = 30 (40-30) = -(10)
+        var damageLeft = -(state.Shield - damage);
+
+        DecreaseShield(damage);
+        
+        if (damageLeft > 0)
+        {
+            DecreaseHealth(damageLeft);
+        }
+    }
+
+    public void AddHealth(int amount)
+    {
+        if(amount == 0) return;
+
+        ref var state = ref _healthState;
+        state.Health = Math.Min(state.Health + amount, state.MaxHealth);
+        state.HasChanged = true;
+    }
+
+    public void DecreaseHealth(int amount)
+    {
+        if(amount == 0) return;
+        
+        ref var state = ref _healthState;
+        state.Health -= amount;
+        state.HasChanged = true;
+        
         if (state.Health <= 0)
         {
             state.Health = 0;
@@ -33,11 +73,29 @@ internal class PlayerHealthComponent : IComponent<PlayerHealthState>
         }
     }
 
-    public void AddHealth(int amount)
+    public void AddShield(int amount)
     {
+        if(amount == 0) return;
+
         ref var state = ref _healthState;
-        state.Health = Math.Min(state.Health + amount, state.MaxHealth);
+        state.Shield = Math.Min(state.Shield + amount, state.MaxShield);
+        state.HasChanged = true;
     }
+
+    public void DecreaseShield(int amount)
+    {
+        if(amount == 0) return;
+        
+        ref var state = ref _healthState;
+        state.Shield -= amount;
+        state.HasChanged = true;
+        
+        if (state.Shield <= 0)
+        {
+            state.Shield = 0;
+        }
+    }
+
 
     public PlayerHealthState State => _healthState;
 }
